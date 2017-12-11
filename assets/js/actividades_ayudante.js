8d49c0497311f61d617a96dc65725a0ebfe7cd32
Kevin Palacios <kopalaci@espol.edu.ec>
2017-12-11 05:36:08 +0100

    Update actividades_ayudante.js

--- assets/js/actividades_ayudante.js
+++ assets/js/actividades_ayudante.js
@@ -1,23 +1,45 @@
 
-[
-		{
-			"datetime":"14 de nov 14:34",
-			"icon":"http://cidecame.uaeh.edu.mx/lcc/mapa/PROYECTO/libro8/Icono_Registro.png",
-			"title":"Correcion de pregunta",
-			"description":"Kevin Palacios ha puesto un comentario sobre tu pregunta"
-		},
-		{
-			"datetime":"14 de nov 14:34",
-			"icon":"http://cidecame.uaeh.edu.mx/lcc/mapa/PROYECTO/libro8/Icono_Registro.png",
-			"title":"Correcion de pregunta",
-			"description":"Kevin Palacios ha puesto un comentario sobre tu pregunta"
-		},
-		{
-			"datetime":"14 de nov 14:34",
-			"icon":"http://cidecame.uaeh.edu.mx/lcc/mapa/PROYECTO/libro8/Icono_Registro.png",
-			"title":"Tu pregunta fue aprobada",
-			"description":"Kevin Palacios aprobó tu pregunta"
-		},
-		
-	]
-  
+function cargarActividades() {
+  $.getJSON('data/actividades_estudiante.json', function(data) {
+	  console.log(data);
+        $.each(data, function(key, val) {
+			var iconValue = val['icon'];
+			var titleValue = val['title'];
+			var descriptionValue = val['description'];
+			
+			var activity = $('<article></article>');
+            activity.attr('class','actividad');
+			var datetime = $('<time></time>');
+            datetime.attr('class','actividad-date');
+			
+			var activityBody = $('<div></div>');
+            activityBody.attr('class','actividad-body');
+			var image_box = $('<div></div>');
+			var image = $('<img></img>');
+			image.attr('class','notice-icon');
+			var description = $('<div></div>');
+			description.attr('class','actividad-description');
+			var title = $('<a></a>');
+			title.attr('class','title');
+			var content = $('<p></p>');
+			datetime.text(val["datetime"]);
+			image.attr('src', iconValue);
+			title.text(titleValue);
+		    content.text(descriptionValue);
+			description.append(title);
+			description.append(content);
+			image_box.append(image);
+			activityBody.append(image_box);
+			activityBody.append(description);
+			activity.append(datetime);
+			activity.append(activityBody);
+			$('.activitiesbox').append(activity);
+
+            
+        });
+    });
+}
+
+$(window).on('load',function(){
+	cargarActividades();
+})
